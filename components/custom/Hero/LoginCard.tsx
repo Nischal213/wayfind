041ade1784b9cc1105f6f19adf036950e4647df5
Nischal213 <xxwassupboixx@gmail.com>
2026-05-02 13:29:52 +0100

    Made login card look nicer

--- components/custom/Hero/LoginCard.tsx
+++ components/custom/Hero/LoginCard.tsx
@@ -3,7 +3,32 @@ import { SignIn } from "@clerk/nextjs"
 export const LoginCard = () => {
   return (
     <div className="flex w-[50%] justify-center items-center bg-[#262624]">
-        <SignIn></SignIn>
+      <SignIn appearance={{
+        variables: {
+          colorPrimary: "#2563eb",
+          colorNeutral: "#30302E",
+          colorForeground: "#EDEDED",
+          colorBackground: "#30302E",
+          colorInputForeground: "#EDEDED",
+          colorInput: "#454543",
+        },
+        elements: {
+          socialButtonsBlockButton : {
+            background: "#454543",
+            "&:hover" : {
+              background: "#262624"
+            }
+          },
+          socialButtonsBlockButtonText__google: {
+            color: '#ededed',
+          },
+          formButtonPrimary : {
+            "&:hover" : {
+              background: "#1d4ed8"
+            }
+          }
+        }
+        }} />
     </div>
   )
 }
